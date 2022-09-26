2013df8c54567adf837f29ef263dd9deab2dd495
Viktoria <zaharova181114@gmail.com>
2022-09-26 13:29:55 +0300

    add product

--- app/js/common.js
+++ app/js/common.js
@@ -19,11 +19,6 @@ $('.main-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 });
 
-// $('.btn-search-mobile').on('click', function (e) {
-//     e.preventDefault();
-//     $('.form-search-mobile').fadeToggle();
-// });
-
 $('.dropdown-menu .dropdown-menu-list .dropdown').on('click', function(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -52,11 +47,15 @@ $(document).ready(function () {
             "ru", "sm", "rs", "sk", "si", "es", "se", "ch", "ua", "gb"],
     });
 
-    // var input3 = document.querySelector(".phone-input3");
-    // window.intlTelInput(input3, {
-    //     separateDialCode: true,
-    //     defaultCountry: 'auto'
-    // });
+    var input3 = document.querySelector(".phone-input3");
+    window.intlTelInput(input3, {
+        separateDialCode: true,
+        defaultCountry: 'auto',
+        onlyCountries: ["al", "ad", "at", "by", "be", "ba", "bg", "hr", "cz", "dk",
+            "ee", "fo", "fi", "fr", "de", "gi", "gr", "va", "hu", "is", "ie", "it", "lv",
+            "li", "lt", "lu", "mk", "mt", "md", "mc", "me", "nl", "no", "pl", "pt", "ro",
+            "ru", "sm", "rs", "sk", "si", "es", "se", "ch", "ua", "gb"],
+    });
     //
     // var input4 = document.querySelector(".phone-input4");
     // window.intlTelInput(input4, {
@@ -159,20 +158,35 @@ $('.slider-range4').slider({
 $(".dec7").val($(".slider-range4").slider("values", 0) + ' ' + '€');
 $(".dec8").val($(".slider-range4").slider("values", 1) + ' ' + '€');
 
-// $('.slider-range5').slider({
-//     range: "min",
-//     min: 0,
-//     max: 112,
-//     value: 64,
-//     classes: {
-//         "ui-slider-handle": "ui-corner-all"
-//     },
-//     slide: function (event, ui) {
-//         //Поле минимального значения
-//         $(".dec4").val(ui.value + ' ' + 'мес.');
-//     }
-// });
-// $(".dec4").val($(".slider-range5").slider("value") + ' ' + 'мес.');
+$('.slider-range5').slider({
+    range: "min",
+    min: 0,
+    max: 20000,
+    value: 11800,
+    classes: {
+        "ui-slider-handle": "ui-corner-all"
+    },
+    slide: function (event, ui) {
+        //Поле минимального значения
+        $(".dec9").val(ui.value + ' ' + '€');
+    }
+});
+$(".dec9").val($(".slider-range5").slider("value") + ' ' + '€');
+
+$('.slider-range6').slider({
+    range: "min",
+    min: 0,
+    max: 90,
+    value: 84,
+    classes: {
+        "ui-slider-handle": "ui-corner-all"
+    },
+    slide: function (event, ui) {
+        //Поле минимального значения
+        $(".dec10").val(ui.value + ' ' + 'мес.');
+    }
+});
+$(".dec10").val($(".slider-range6").slider("value") + ' ' + 'мес.');
 
 
 $('.interested-slider').slick({
@@ -283,6 +297,78 @@ $('.btn-search').on('click', function (e) {
 });
 
 
+$('.product-gallery').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: true,
+    asNavFor: '.product-gallery-preview',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
+
+$('.product-gallery-preview').slick({
+    slidesToShow: 6,
+    arrows: false,
+    asNavFor: '.product-gallery',
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 6,
+            }
+        },
+        {
+            breakpoint: 650,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 4,
+            }
+        }
+    ]
+});
+
+// hidden list > 5
+$('.sidebar .sidebar-box').each(function () {
+    if ($(this).find('ul li').length > 5) {
+        $(this).find('ul li').slice(5).hide();
+        $(this).find('.sidebar-box__body').append('<a href="#" class="color-accent load-more-wrap">Показать все</a>');
+    }
+
+});
+
+// hidden list > 5
+
+// show list all
+$('.load-more-wrap').on('click', function (e) {
+    e.preventDefault();
+    $(this).parents('.sidebar-box').find('ul li:hidden').slice(0, 5).slideDown();
+
+    var onBlock = $(this).parents('.sidebar-box').find('ul li:hidden').length;
+    if(onBlock <= 0) {
+        $(this).hide();
+    }
+});
+
+// show list all
+
 
 
 
@@ -341,41 +427,6 @@ $('.reviews-slider').slick({
     ]
 });
 
-$('.product-gallery').slick({
-    slidesToShow: 1,
-    fade: true,
-    arrows: false,
-    asNavFor: '.product-gallery-preview'
-});
-
-$('.product-gallery-preview').slick({
-    slidesToShow: 6,
-    arrows: true,
-    asNavFor: '.product-gallery',
-    focusOnSelect: true,
-    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 4,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 5,
-            }
-        },
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 3,
-            }
-        }
-    ]
-});
 
 
 // slick active
